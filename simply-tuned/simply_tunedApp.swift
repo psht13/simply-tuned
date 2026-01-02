30a08bd3a58485a2693c14b346179602234f16b4
psht13 <pashtet.gm@gmail.com>
2026-01-02 18:03:31 +0200

    Initial Commit

---
+++ simply-tuned/simply_tunedApp.swift
@@ -0,0 +1,17 @@
+//
+//  simply_tunedApp.swift
+//  simply-tuned
+//
+//  Created by main on 02.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct simply_tunedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
